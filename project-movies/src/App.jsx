e17c25f568f7763dc8e239db1fa5516622235fab
DevTDavicho <developer2023Davicho@outlook.es>
2023-10-19 20:11:03 -0500

    Mi primer commit

---
+++ project-movies/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <header>
+      <h1 className='title'> 🍿Peliculas </h1>
+    </header>
+  )
+}
+
+export default App
---
+++ project-movies/src/data/httpClient.js
@@ -0,0 +1,10 @@
+const API="https://www.themoviedb.org/"
+export function get(path){
+    return fetch(API-path,{
+        headers:{
+            Authorization:
+            "Bearer 365f9418702eabd8aadcbbffdd4fa
+        
+
+    })
+}
